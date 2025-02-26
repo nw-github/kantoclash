010100d8edc0d9e2f065e19d3c116a1e2f8da282
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2025-02-25 19:56:00 -0500

    meta: use drizzle-kit pglite driver

--- drizzle-prod.config.ts
+++ drizzle-prod.config.ts
@@ -4,6 +4,5 @@ export default defineConfig({
   out: "./server/db/migrations",
   schema: "./server/db/schema.ts",
   dialect: "postgresql",
-  driver: "pglite",
   dbCredentials: { url: process.env.DB_URL! },
 });
