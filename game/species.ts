8b446d9e16c85b2760da228bd4cf8278fc85bd45
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2025-04-01 11:11:18 -0400

    battle: add gen3 species data

--- game/species.ts
+++ game/species.ts
@@ -1,19 +1,23 @@
 import type {MoveId} from "./moves";
 import type {Stats, Type} from "./utils";
 import __speciesList from "./species.json";
+import abilityList from "./ability.json";
 
 export type Species = {
   readonly dexId: number;
   readonly types: readonly Type[];
   readonly moves: readonly MoveId[];
+  readonly abilities: readonly AbilityId[];
   readonly stats: Stats;
   readonly name: string;
   readonly evolvesFrom?: SpeciesId;
-  readonly evolvesTo?: SpeciesId;
-  /** Male part of Ratio | Ex: 87.5 means 87.5% male, 12.5% female */
+  readonly evolvesTo?: SpeciesId | SpeciesId[];
+  /** Male part of Ratio | Ex: 87.5 means 87.5% male, 12.5% female. Undefined for Gender Unknown */
   readonly genderRatio?: number;
 };
 
 export type SpeciesId = keyof typeof __speciesList;
+export type AbilityId = keyof typeof abilityList;
 
-export const speciesList = __speciesList as any as Record<SpeciesId, Species>;
+export const speciesList = __speciesList as Record<SpeciesId, Species>;
+export {abilityList};
