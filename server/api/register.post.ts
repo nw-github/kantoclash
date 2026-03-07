b85d47e74b0136b4ceb1d2041e119f647c392440
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2026-03-07 06:55:55 -0500

    server: enforce username case-insensitive uniqueness

--- server/api/register.post.ts
+++ server/api/register.post.ts
@@ -1,19 +1,24 @@
 import {userSchema} from "~/utils/schema";
 import {users} from "../db/schema";
-import {eq} from "drizzle-orm";
 
 export default defineEventHandler(async event => {
   const db = useDrizzle();
   const {username, password} = await readValidatedBody(event, userSchema.parse);
-  const prev = await db.select().from(users).where(eq(users.username, username));
-  if (prev.length) {
-    throw createError({statusCode: 409, message: "Username already taken"});
+
+  let user;
+  try {
+    [user] = await db
+      .insert(users)
+      .values({username, password: await hashPassword(password)})
+      .returning({username: users.username, id: users.id})
+      .onConflictDoNothing();
+  } catch {
+    throw createError({statusCode: 500, message: "Internal error occurred"});
   }
 
-  const [user] = await db
-    .insert(users)
-    .values({username, password: await hashPassword(password)})
-    .returning({username: users.username, id: users.id});
+  if (!user) {
+    throw createError({statusCode: 409, message: "Username already taken"});
+  }
 
   await setUserSession(event, {user: {name: user.username, id: String(user.id)}});
   return {};
